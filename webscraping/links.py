7b38acf671804ab7bac604aad0f1ade185d7ccc8
LuciLua <luci.lua81@gmail.com>
2021-05-29 15:25:05 -0300

    :computer: Web Scraping | title Status LinksProjects

--- webscraping/links.py
+++ webscraping/links.py
@@ -4,21 +4,24 @@ import cv2
 
 response = requests.get('https://www.luci-lua.tk/')
 
-print ('Status: ', response.status_code)
+print ( ('\n'+'-'*20+'\n'), 'Status: ', response.status_code)
 
 content = response.content
 
 site = BeautifulSoup(content, 'html.parser')
 
-print(site.prettify())
+# print(site.prettify())
 
 # tag, atributo
 rl = site.find('div', attrs={'class': 'left'})
 
 p = rl.find('p')
 h1 = rl.find('h1')
+
+titlePage = site.find('title')
+print(titlePage.text, ('\n'+'-'*20+'\n'))
  
-print(h1.text ,p.text)
+# print(h1.text ,p.text)
 
 projetos = site.findAll('div', attrs={'class': 'btn-h'})
 
@@ -26,3 +29,4 @@ title = site.findAll('a', attrs={'class': 'acc'})
 
 for title in title:
    print(title.text,':',title['href'])
+
