e640139f519b8ca0222d3bab3c34e899f68e0d7c
LuciLua <luci.lua81@gmail.com>
2022-10-19 00:11:28 -0300

    :star2: hostname test

--- testUbuntu/test.py
+++ testUbuntu/test.py
@@ -1,4 +1,13 @@
+import os
+import socket
+
 name = input('Write your name here: ')
+
+def hst():
+    if name == 'luci':
+        return socket.gethostname()
+    return 'just nothing'
+
 print('hi, {}. How are you?'.format(name))
 print('[...] Waiting a little more, please')
-input('Alrigth, press any key for continue')
+input('Alrigth, press any key for continue... {}'.format(hst()))
